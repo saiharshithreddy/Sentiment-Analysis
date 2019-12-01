02ef1c7b36e5e882dbba0b0f06b930202d801f50
harshithreddyhr9 <shrshthreddy@gmail.com>
2019-12-01 15:37:07 -0800

    rcnn

--- neural nets/yelp_data.py
+++ neural nets/yelp_data.py
@@ -1,44 +1,74 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 11 22:00:41 2019
-
-@author: saiharshith
-"""
-import numpy as np
 import torch
+from torchtext import data
+from torchtext.vocab import Vectors
 import pandas as pd
-from torch.utils.data import Dataset
-from sklearn.model_selection import train_test_split
-from torch.utils.data.sampler import SequentialSampler
-from torch.autograd import Variable
+import numpy as np
+from sklearn.metrics import accuracy_score
+from nltk.tokenize import sent_tokenize, word_tokenize
 
-# Define your custom dataset class to be loaded by pytorch's dataloader
-
-class YelpDataset(Dataset):
-    def __init__(self, vocab, data, set_train=None):
-        self.train_data, self.test_data = train_test_split(data, train_size=0.70, 
-                                                 test_size=0.3, random_state=0)
-        if set_train:
-            self.sentence = self.train_data['sentence'].tolist()
-            self.sentiment = self.train_data['sentiment'].tolist()
+class Yelp_Dataset(object):
+    def __init__(self, config):
+        self.config = config
+        self.train_iterator = None
+        self.test_iterator = None
+        self.val_iterator = None
+        self.vocab = []
+        self.word_embeddings = {}
+    
+    def load_data(self, w2v_file, train_df, test_df):
+        tokenizer = lambda sent: [x for x in word_tokenize(sent) if x != " "]
         
+        # Creating Field for data
+        TEXT = data.Field(sequential=True, tokenize=tokenizer, lower=True)
+        LABEL = data.Field(sequential=False, use_vocab=False)
+        datafields = [("text",TEXT),("label",LABEL)]
+        
+        # Load data from pd.DataFrame into torchtext.data.Dataset
+        
+        train_examples = [data.Example.fromlist(i, datafields) for i in train_df.values.tolist()]
+        train_data = data.Dataset(train_examples, datafields)
+        
+        test_examples = [data.Example.fromlist(i, datafields) for i in test_df.values.tolist()]
+        test_data = data.Dataset(test_examples, datafields)
+        
+        
+        
+        train_data, val_data = train_data.split(split_ratio=0.8)
+        
+        TEXT.build_vocab(train_data, vectors=Vectors(w2v_file))
+        self.word_embeddings = TEXT.vocab.vectors
+        self.vocab = TEXT.vocab
+        
+        self.train_iterator = data.BucketIterator(
+            (train_data),
+            batch_size=self.config.batch_size,
+            sort_key=lambda x: len(x.text),
+            repeat=False,
+            shuffle=True)
+        
+        self.val_iterator, self.test_iterator = data.BucketIterator.splits(
+            (val_data, test_data),
+            batch_size=self.config.batch_size,
+            sort_key=lambda x: len(x.text),
+            repeat=False,
+            shuffle=False)
+        
+        print ("Loaded {} training examples".format(len(train_data)))
+        print ("Loaded {} test examples".format(len(test_data)))
+        print ("Loaded {} validation examples".format(len(val_data)))
+
+
+def get_accuracy(model, iterator):
+    all_preds = []
+    all_y = []
+    for idx,batch in enumerate(iterator):
+        if torch.cuda.is_available():
+            x = batch.text.cuda()
         else:
-            self.sentence = self.test_data['sentence'].tolist()
-            self.sentiment = self.test_data['sentiment'].tolist()
-        
-        self.vocab = vocab
-        
-        
-    def __len__(self):
-        return len(self.dataset)
-    
-    def __getitem__(self, idx):
-        data_idx = {'sentence': [], 'sentiment': self.category[idx]}
-        for i, word in enumerate(self.category[idx].split()):
-            data_idx['sentiment'].append(self.vocab.get_idx(word))
-    
-        return data_idx  # return index rather than word
-    
-    def test(self, idx):
-        return self.__getitem__(idx)
-        
+            x = batch.text
+        y_pred = model(x)
+        predicted = torch.max(y_pred.cpu().data, 1)[1] + 1
+        all_preds.extend(predicted.numpy())
+        all_y.extend(batch.label.numpy())
+    score = accuracy_score(all_y, np.array(all_preds).flatten())
+    return score
