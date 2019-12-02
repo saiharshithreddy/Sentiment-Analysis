fec6fabc32a5ecc20eca451d6fd412830b807de6
harshithreddyhr9 <shrshthreddy@gmail.com>
2019-12-02 02:07:20 -0800

    added lemmatization and contractions to preprocessing

--- data_preprocessing/cleaning_text.py
+++ data_preprocessing/cleaning_text.py
@@ -6,10 +6,21 @@ from multiprocessing import  Pool
 import logging
 import nltk
 nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+from contractions import CONTRACTION_MAP
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+
+
 
 df = pd.read_csv('review.csv')
-#df = df.iloc[:220,:]
+
 print("Data loaded")
 df_t = df.loc[:,['text', 'stars']]
 df_t = df_t.dropna()
@@ -21,18 +32,11 @@ label_encoder = LabelEncoder()
 
 df_t['sentiment']= label_encoder.fit_transform(df_t['sentiment'])
 
-df_t.sentiment = df_t.sentiment.astype('int64')
+df_t.sentiment = df_t.sentiment.astype('int8')
 
 
-
-
-
+# Using 24 cores in the HPC
 def parallelize(df, func, n_cores=24):
-    '''
-    The dataframe is split into n_cores and availability_filter is performed on each sub dataframe
-    n_cores: No of processors (96 in this env)
-    n_cores is taken as 10 as it takes less time to split the given data.
-    '''
     df_split = np.array_split(df, n_cores)
     pool = Pool(n_cores)
     df = pd.concat(pool.map(func, df_split))
@@ -40,6 +44,7 @@ def parallelize(df, func, n_cores=24):
     pool.join()
     return df
   
+# To check the number of rows preprocessed
 def log(file_name):
     logger = logging.getLogger()
     fhandler = logging.FileHandler(filename=file_name, mode='a')
@@ -48,8 +53,6 @@ def log(file_name):
     logger.addHandler(fhandler)
     logger.setLevel(logging.DEBUG)
 log('log.txt')
-
-
 
 def clean_text(text):
     '''Remove unwanted characters, stopwords, and format the text to create fewer nulls word embeddings'''
@@ -68,15 +71,54 @@ def clean_text(text):
     text = re.sub(r'\'', ' ', text)
     
     
+    
+    contractions_pattern = re.compile('({})'.format('|'.join(CONTRACTION_MAP.keys())), 
+                                      flags=re.IGNORECASE|re.DOTALL)
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = CONTRACTION_MAP.get(match)\
+                                if CONTRACTION_MAP.get(match)\
+                                else CONTRACTION_MAP.get(match.lower())                       
+        expanded_contraction = first_char+expanded_contraction[1:]
+        return expanded_contraction
+        
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = re.sub("'", "", expanded_text)
+       
     # Optionally, remove stop words
     
-    text = text.split()
+    text = expanded_text.split()
     stops = set(stopwords.words("english"))
     text = [w for w in text if not w in stops]
     text = " ".join(text)
     logging.info("sentence cleaned")
+    # Lemmatization
+    
+    wordnet_lemmatizer = WordNetLemmatizer()
 
-    return text
+    def get_tag(tag):
+        if tag.startswith('J'):
+            return wordnet.ADJ
+        elif tag.startswith('V'):
+            return wordnet.VERB
+        elif tag.startswith('N'):
+            return wordnet.NOUN
+        elif tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return ''
+
+    text_result = []
+    tokens = word_tokenize(text)  # Generate list of tokens
+    tagged = pos_tag(tokens)
+    for t in tagged:
+        try:
+            text_result.append(wordnet_lemmatizer.lemmatize(t[0], get_tag(t[1][:2])))
+        except:
+            text_result.append(wordnet_lemmatizer.lemmatize(t[0]))
+    paragraph = " ".join(str(x) for x in text_result)
+    return paragraph
 
 def loop(data):
     #applies the function on each value of the pandas series
@@ -90,19 +132,5 @@ df_t['sentence'] = pd.Series(text)
 df_t['length'] = df_t['sentence'].str.len()
 df_test = df_t.loc[df_t['length'] >= 20]
 df_test.drop(['length'],axis=1,inplace=True)
-df_test.to_csv('yelp_reviews.csv', index=False)
-df_a = df_test.iloc[:1000000,:]
-df_b = df_test.iloc[1000000:2000000,:]
-df_c = df_test.iloc[2000000:3000000,:]
-df_d = df_test.iloc[3000000:4000000,:]
-df_e = df_test.iloc[4000000:5000000,:]
-df_f = df_test.iloc[5000000:6000000,:]
-df_g = df_test.iloc[6000000::,:]
+df_test.to_pickle('cleaned_yelp_reviews.pickle')
 
-df_a.to_pickle('yelp_data1.pkl')
-df_b.to_pickle('yelp_data2.pkl')
-df_c.to_pickle('yelp_data3.pkl')
-df_d.to_pickle('yelp_data4.pkl')
-df_e.to_pickle('yelp_data5.pkl')
-df_f.to_pickle('yelp_data6.pkl')
-df_g.to_pickle('yelp_data7.pkl')
